5633c11f829fb044c3825621c9c2a52d2b766d45
samirsukla <sk.sukla@outlook.com>
2020-04-03 13:44:22 +0530

    Added the project Structure

---
+++ src/main/java/com/qa/base/BaseClass.java
@@ -0,0 +1,27 @@
+package com.qa.base;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class BaseClass {
+
+	public static String baseURL;
+	public static Properties prop;
+	public static String tokenId;
+	
+
+//This is the baseclass constructor which will load the properties file, so whichever class extends this baseclass 
+//will have to create a default constructor to call the super class constructor.
+	public BaseClass() throws IOException {
+		String fileLoc = System.getProperty("user.dir")+"\\src\\main\\java\\com\\qa\\config\\config.properties";
+		prop = new Properties();
+		FileInputStream fis = new FileInputStream(new File(fileLoc)); //loading the properties file
+		prop.load(fis);
+		baseURL = prop.getProperty("baseUrl");
+		
+
+	}
+}
+
