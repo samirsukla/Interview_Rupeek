0795393b6fca3e3be8fb641329ffd85dfbe69d67
samirsukla <sk.sukla@outlook.com>
2020-04-03 13:35:18 +0530

    First Commit-Created the Repository

---
+++ src/main/java/com/APITest_Rupeek/App.java
@@ -0,0 +1,13 @@
+package com.APITest_Rupeek;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
